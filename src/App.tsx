58e6001f0696267ed7e32e8fd41590442a63a35b
William <lowrwilliam@gmail.com>
2023-12-08 18:38:42 -0600

    fixed home page links

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,9 @@ import {
   Link,
 } from 'react-router-dom'
 import Home from './Components/Landing Page/Home'
-import Header from './Components/Header'
 import Navbar from './Components/Navbar'
+import About from './Components/About/About'
+import Projects from './Components/Projects/Projects'
 
 function App() {
 
@@ -18,19 +19,10 @@ function App() {
       <Router>
       <div className='w-full h-[120rem]'>
         <Navbar/>
-        <h2><Header/></h2>
-        <nav>
-          <ul>
-            <li><Link to={'/'} className="nav-link"> Home </Link></li>
-            <li><Link to={'/projects'} className="nav-link">Projects</Link></li>
-            <li><Link to={'/about'} className="nav-link">About</Link></li>
-          </ul>
-        </nav>
-        <hr />
         <Routes>
           <Route path='/' element={<Home firstName={'William'} lastName={'Lowrimore'}/>} />
-      {/*     <Route path='/projects' element={<Projects/>} />
-          <Route path='/about' element={<qAbout/>} /> */}
+          <Route path='/projects' element={<Projects/>}/>
+          <Route path='/about' element={<About/>} />
         </Routes>
       </div>
     </Router>
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,11 +1,6 @@
 import "./Header.css";
 
 export const Header = () => {
-    return (
-      <header>
-        <h1 className="website-header">WL</h1>
-      </header>
-    );
   }
   
 export default Header;
