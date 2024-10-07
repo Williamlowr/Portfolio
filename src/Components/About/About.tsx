eefb2acdf7c7de2b9ea0e55a99fe1a9ce8710ed6
William <lowrwilliam@gmail.com>
2024-10-07 11:13:14 -0500

    update content

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,40 +1,47 @@
 export default function About() {
   return (
-    <div className="bg-gray-100">
+    <div className="">
       {/* Introduction Section */}
-      <div className="py-12 px-4 md:px-12">
-        <h2 className="text-4xl font-bold text-gray-800 mb-4">About Me</h2>
-        <p className="text-xl text-gray-600">
-          Hello! I'm William Lowrimore, a passionate frontend developer based in Tyler, Texas. I love creating interactive and responsive web applications
-          that provide an engaging user experience.
+      <div className="py-12 px-4 md:px-12 bg-gray-700">
+        <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+        <p className="text-xl text-white">
+          Hello, I'm William Lowrimore, a computer science student based in Tyler, Texas. Right now, I especially enjoy 
+          creating interactive web applications, though my interests and niche are still being fully explored.
         </p>
         {/* Add more personal details or a story here */}
       </div>
 
       {/* Skills or Experience Section */}
-      <div className="py-12 px-4 md:px-12 bg-white">
-        <h3 className="text-3xl font-bold text-gray-800 mb-4">
+      <div className="py-12 px-4 md:px-12 bg-gray-800">
+        <h3 className="text-3xl font-bold text-white mb-4">
           Skills & Experience
         </h3>
-        <p className="text-xl text-gray-600">
-          With a background in x, I have developed a strong
-          skill set in x, y, and z. My projects range
-          from simple websites to complex web applications.
+        <p className="text-xl text-white">
+          With a background in insurance adjusting, as well as 
+          a degree in CS and certificate in Data Science, I feel I bring a lot of
+          unique skills to the table. My projects range
+          from websites to Java servers to WebGL and more.
         </p>
         {/* List more skills or experiences here */}
       </div>
 
       {/* Contact Section */}
-      <div className="py-12 px-4 md:px-12">
-        <h4 className="text-3xl font-bold text-gray-800 mb-4">Contact Me</h4>
-        <p className="text-xl text-gray-600 mb-4">
-          Interested in working together? I'd love to hear from you!
+      <div className="py-12 px-4 md:px-12 bg-gray-700">
+        <h4 className="text-3xl font-bold text-white mb-4">Contact Me</h4>
+        <p className="text-xl text-white mb-4">
+          Interested in working together? I'd love to hear from you! You can reach me via email below, or feel free to connect on LinkedIn.
         </p>
         <a
-          href="mailto:your.email@example.com"
+          href="mailto:lowrwilliam@gmail.com"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
           Email Me
+        </a>
+        <a
+          href="https://www.linkedin.com/in/william-lowrimore/"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 ml-6 px-4 rounded"
+        >
+          LinkedIn
         </a>
         {/* Add more contact options if necessary */}
       </div>
--- src/Components/Resume/Resume.tsx
+++ src/Components/Resume/Resume.tsx
@@ -2,14 +2,14 @@ import resume from "../../assets/ResumeRecent.pdf";
 
 export default function ResumeDownloadPage(): JSX.Element {
   return (
-    <div className="bg-gray-100 h-screen flex flex-col items-center justify-center">
+    <div className=" h-screen flex flex-col items-center justify-center">
       {/* Introduction and Summary */}
       <div className="text-center mb-6">
-        <h1 className="text-4xl font-bold text-gray-800">William Lowrimore</h1>
-        <p className="text-xl text-gray-600 mt-2">Frontend/UI Developer</p>
-        <p className="mt-4 text-gray-600 max-w-md mx-auto">
+        <h1 className="text-4xl font-bold text-white">William Lowrimore</h1>
+        <p className="text-xl text-white mt-2">Frontend Developer</p>
+        <p className="mt-4 text-white max-w-md mx-auto">
           I specialize in creating visually appealing and user-friendly
-          websites. My expertise lies in HTML, CSS, JavaScript, and modern
+          websites. My expertise lies in HTML, CSS, JavaScript, and especially modern
           frameworks like React.
         </p>
       </div>
