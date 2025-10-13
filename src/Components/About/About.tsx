775719c744d6cf6b9ed3e1471fdb9ab58aa88012
William <lowrwilliam@gmail.com>
2025-10-13 02:19:09 -0500

    Reworked Projects Tab

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <div className="">
       {/* Introduction Section */}
-      <div className="py-12 px-4 md:px-12 bg-gray-700">
+      <div className="py-12 px-4 md:px-12 bg-gray-700/70">
         <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
         <p className="text-xl text-white">
           Hello, I'm William Lowrimore, a computer science student based in Tyler, Texas. Right now, I especially enjoy 
@@ -10,11 +10,11 @@ export default function About() {
 
           This site was made using React and Tailwind featuring reusable components, a clean file structure, and an easily expandable/customizable codebase.
         </p>
-        {/* Add more personal details or a story here */}
+        {/* To add personal details later */}
       </div>
 
-      {/* Skills or Experience Section */}
-      <div className="py-12 px-4 md:px-12 bg-gray-800">
+      {/* Skills/Experience Section */}
+      <div className="py-12 px-4 md:px-12 bg-gray-700/20">
         <h3 className="text-3xl font-bold text-white mb-4">
           Skills & Experience
         </h3>
@@ -24,11 +24,11 @@ export default function About() {
           unique skills to the table. My projects range
           from websites to Java servers to WebGL and more.
         </p>
-        {/* List more skills or experiences here */}
+        {/* List more later */}
       </div>
 
       {/* Contact Section */}
-      <div className="py-12 px-4 md:px-12 bg-gray-700">
+      <div className="py-12 px-4 md:px-12 bg-gray-700/70">
         <h4 className="text-3xl font-bold text-white mb-4">Contact Me</h4>
         <p className="text-xl text-white mb-4">
           Interested in working together? I'd love to hear from you! You can reach me via email below, or feel free to connect on LinkedIn.
@@ -45,7 +45,6 @@ export default function About() {
         >
           LinkedIn
         </a>
-        {/* Add more contact options if necessary */}
       </div>
     </div>
   );
